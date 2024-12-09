0da42f4244b53de92427493022679c39ac81e2f2
Leandrho <leandro.coder01@gmail.com>
2024-12-09 12:54:34 -0300

    add css for App

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,25 @@
 import { IPInfo } from "./components/IPInfo"
 import { IPSearch } from "./components/IPSearch"
 
+import c from "./App.module.css"
+
 function App() {
   return (
     <>
-      <div className="">
+      <div className={c.container}>
         <header>
           <h1>IP Address Tracker</h1>
           <IPSearch />
         </header>
         <main>
-          <IPInfo />
+          <figure className="map">
+            <img src="" alt="location map" />
+          </figure>
         </main>
+        <div>
+          <IPInfo />
+        </div>
+        
       </div>
     </>
   )
