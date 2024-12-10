b2a3740304c367f83483da158b3ad5a383ef1b08
Leandrho <leandro.coder01@gmail.com>
2024-12-10 17:47:28 -0300

    fix svg

--- src/components/IPSearch.jsx
+++ src/components/IPSearch.jsx
@@ -6,7 +6,7 @@ export const IPSearch = () => {
       <div className={c.search__box}>
         <input type="text" name="search_input" id="search_input" placeholder="Search for any IP Address or domain"/>
         <button type="submit">
-          <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" stroke-width="3" d="M2 1l6 6-6 6"/></svg>
+          <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6"/></svg>
         </button>
       </div>
     </form>
