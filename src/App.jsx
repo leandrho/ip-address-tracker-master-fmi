b772c22a04a5671b966e237ad85ec432afd472a2
Leandrho <leandro.coder01@gmail.com>
2024-12-10 17:37:56 -0300

    working on structure

--- src/App.jsx
+++ src/App.jsx
@@ -7,19 +7,21 @@ function App() {
   return (
     <>
       <div className={c.container}>
-        <header>
-          <h1>IP Address Tracker</h1>
+        <section className={c.absolute}>
+          <h1 className={c.title}>IP Address Tracker</h1>
           <IPSearch />
-        </header>
+          <IPInfo />
+        </section>
         <main>
+          <picture className={c.back__pic}>
+              <source media="(max-width: 580px)" srcSet="./images/pattern-bg-mobile.png" />
+              <source media="(min-width: 581px)" srcSet="./images/pattern-bg-desktop.png" />
+              <img src="./images/desktop-image-hero-1.jpg" alt="" className={c.back__img}/>
+          </picture>
           <figure className="map">
             <img src="" alt="location map" />
           </figure>
-        </main>
-        <div>
-          <IPInfo />
-        </div>
-        
+        </main>        
       </div>
     </>
   )
