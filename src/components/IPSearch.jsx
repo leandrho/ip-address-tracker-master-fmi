cdd2a724f0d05b495d00709a39a5a7ca44b23f66
Leandrho <leandro.coder01@gmail.com>
2024-12-12 10:12:32 -0300

    ...

--- src/components/IPSearch.jsx
+++ src/components/IPSearch.jsx
@@ -4,8 +4,8 @@ export const IPSearch = () => {
   return (
     <form onSubmit={()=>{}} className="search__form">
       <div className={c.search__box}>
-        <input type="text" name="search_input" id="search_input" placeholder="Search for any IP Address or domain"/>
-        <button type="submit">
+        <input type="text" name="search_input" id="search_input" placeholder="Search for any IP Address or domain" className={c.input}/>
+        <button type="submit" className={c.btn}>
           <svg xmlns="http://www.w3.org/2000/svg" width="11" height="14"><path fill="none" stroke="#FFF" strokeWidth="3" d="M2 1l6 6-6 6"/></svg>
         </button>
       </div>
