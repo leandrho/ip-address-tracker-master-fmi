ba1bcf820c68a142a143de47a3abcb6aa15d4c5c
Leandrho <leandro.coder01@gmail.com>
2024-12-12 13:07:45 -0300

    structuring IPInfo

--- src/components/IPInfo.jsx
+++ src/components/IPInfo.jsx
@@ -1,6 +1,24 @@
+import css from './IPInfo.module.css'
 
 export const IPInfo = () => {
   return (
-    <div>IPInfo</div>
+    <div className={css.container}>
+        <div className={css.box}>
+            <h2>ip address</h2>
+            <p>192.212.174.101</p>
+        </div>
+        <div className={css.box}>
+            <h2>location</h2>
+            <p>Brooklyn, NY 10001</p>
+        </div>
+        <div className={css.box}>
+            <h2>timezone</h2>
+            <p>UTC -05:00</p>
+        </div>
+        <div className={css.box}>
+            <h2>isp</h2>
+            <p>SpaceX Starlink</p>
+        </div>
+    </div>
   )
 }
