19ffbd2f7f542a6269441ee73eb1ef65d3b07769
Leandrho <leandro.coder01@gmail.com>
2025-01-08 12:55:41 -0300

    change get method

--- src/services/index.js
+++ src/services/index.js
@@ -1,10 +1,21 @@
 import axios from "axios";
 
-export const getLatLong = async (ip) => {
+export const getIPInfo = async (ip) => {
     const url =`https://geo.ipify.org/api/v2/country,city?apiKey=${import.meta.env.VITE_APP_IPKEY}&ipAddress=${ip}`
     try{
-        const {data: {location: {lat, lng}}} = await axios.get(url);
-        return {lat, lng};
+        const {data: {isp, location}} = await axios.get(url);
+        const {lat, lng, city, timezone, country, region} = location;
+        const ret = {
+            isp,
+            city,
+            country,
+            timezone,
+            region,
+            lat, lng,
+            curIP: ip
+        }
+        console.log(ret)
+        return ret;
     }
     catch(err){
         throw new Error("Error getting data: ", err);
