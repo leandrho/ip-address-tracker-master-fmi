690b75a71d3c3dfff7e1f16b4f93bc1fd0f1af72
Leandrho <leandro.coder01@gmail.com>
2025-01-08 12:56:39 -0300

    change context

--- src/context/IPContext.jsx
+++ src/context/IPContext.jsx
@@ -3,16 +3,31 @@ import { createContext, useState } from "react";
 export const IPContext = createContext({
     ip: '',
     setIP: () => {},
-    ll: { lat: 0, lng: 0 },
-    setLL: () => {},
+    ipInfo: {
+        isp: '',
+        city: '',
+        country: '',
+        timezone: '',
+        region: '',
+        lat: 0, lng: 0,
+        curIP: ''
+    },
+    setIpInfo: () => {},
   });
 
 export const IPProvider = ({children}) => {
-    const [ip, setIP] = useState('');
-    const [ll, setLL] = useState({lat: 0, lng: 0});
-
+    const [ip, setIP] = useState('192.212.174.101');
+    const [ipInfo, setIpInfo] = useState({
+        isp: '',
+        city: '',
+        country: '',
+        timezone: '',
+        region: '',
+        lat: 0, lng: 0,
+        curIP: ''
+    });
     return (
-        <IPContext.Provider value={{ip, setIP, ll, setLL}}> 
+        <IPContext.Provider value={{ip, setIP, ipInfo, setIpInfo}}> 
             {children}
         </IPContext.Provider>
     )
