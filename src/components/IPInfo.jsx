27da0a990873014ff4362474dcca84a3480a1d0e
Leandrho <leandro.coder01@gmail.com>
2025-01-08 12:58:25 -0300

    dynamic info

--- src/components/IPInfo.jsx
+++ src/components/IPInfo.jsx
@@ -1,23 +1,26 @@
+import { useIP } from '../hooks/useIP';
 import css from './IPInfo.module.css'
 
+
 export const IPInfo = () => {
+    const {ipInfo, ip} = useIP();
   return (
     <div className={css.container}>
         <div className={css.box}>
             <h2>ip address</h2>
-            <p>192.212.174.101</p>
+            <p>{ip}</p>
         </div>
         <div className={css.box}>
             <h2>location</h2>
-            <p>Brooklyn, NY 10001</p>
+            <p>{ipInfo.region}</p>
         </div>
         <div className={css.box}>
             <h2>timezone</h2>
-            <p>UTC -05:00</p>
+            <p>UTC {ipInfo.timezone}</p>
         </div>
         <div className={css.box}>
             <h2>isp</h2>
-            <p>SpaceX Starlink</p>
+            <p>{ipInfo.isp}</p>
         </div>
     </div>
   )
